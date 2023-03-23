99908ca9acb2f1439f8046d29ad4bb554ff1e36d
Jinwoo Oh <oh7895@hanyang.ac.kr>
2023-03-23 14:52:30 +0900

    fourth commit

---
+++ javascript/var1.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = 2;
+let myName = "Jinwoo"; /* const는 상수이므로 다른 값으로 바꿀 수 없다 */
+                       /* let은 다른 값으로 바꿀 수 있다.*/
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello " + myName);
+
+myName = "OH Jinwoo";
+
+console.log("your new name is " + myName);
---
+++ javascript/var2.js
@@ -0,0 +1,3 @@
+const amIFat = null; /* null은 아무것도 없음으로 채워진 상태를 의미한다. */
+let something; /* 값을 지정하지 않으면 변수는 존재하는데 그 안에 값이 없는 상태이다. (undefined)*/
+console.log(amIFat, something);
